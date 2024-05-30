f053aaa897819581c774230173eed305dafea3af
BirWri <55280458+BirWri@users.noreply.github.com>
2024-05-30 12:19:24 +0200

    user input

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+drinks_list= Menu()
+user_choice = input(f"What would you like? ({drinks_list.get_items()}): ").lower()
+
+
