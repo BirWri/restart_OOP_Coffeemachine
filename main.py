417c428bc8b7731e5909196d057f932fc93451c7
BirWri <55280458+BirWri@users.noreply.github.com>
2024-05-30 12:29:32 +0200

    user input different conditions

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-drinks_list= Menu()
-user_choice = input(f"What would you like? ({drinks_list.get_items()}): ").lower()
+drinks_list = Menu()
+report = CoffeeMaker()
+profit = MoneyMachine()
+
+
+machine_on = True
+
+while machine_on:
+
+    user_choice = input(f"What would you like? ({drinks_list.get_items()}): ").lower()
+
+    if user_choice == "report":
+        report.report()
+        profit.report()
+    elif user_choice == "off":
+        machine_on = False
+    elif not drinks_list.find_drink(user_choice):
+        pass
+
+
 
 
